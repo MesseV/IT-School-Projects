887371cc1b3649d40fa45991724703467637a326
MesseV <vlad.mesesan1@gmail.com>
2021-02-15 20:08:22 +0200

    +++

--- BlocLesson/src/com/company/Bloc.java
+++ BlocLesson/src/com/company/Bloc.java
@@ -5,8 +5,15 @@ public class Bloc {
     private String bloc;
     private int etaje;
 
-    public Bloc(int etaje, String scara) {
+    public Bloc(int etaje, String scara, String bloc) {
         this.etaje = etaje;
         this.scara = scara;
+        this.bloc = bloc;
     }
+
+    public String getAddress () {
+        String adresa = this.bloc + " " + this.scara;
+        return adresa;
+    }
+
 }
--- BlocLesson/src/com/company/Main.java
+++ BlocLesson/src/com/company/Main.java
@@ -3,7 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	Bloc blocA1 = new Bloc(3, "A45");
+	Bloc blocA1 = new Bloc(3, "S45", "A1");
+	Bloc blocB1 = new Bloc(5, "S30", "B1");
+
+        System.out.println("Adresa:" + blocA1.getAddress());
 
 
     }
