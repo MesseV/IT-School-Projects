1614f1433431224f81bc339022c6be22b17ad8cf
MesseV <vlad.mesesan1@gmail.com>
2021-02-15 19:56:23 +0200

    created bloc
    +added constructors

---
+++ BlocLesson/src/com/company/Bloc.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Bloc {
+    private String scara;
+    private String bloc;
+    private int etaje;
+
+    Bloc(int etaje, String scara) {
+        this.etaje = etaje;
+        this.scara = scara;
+    }
+}
--- BlocLesson/src/com/company/Main.java
+++ BlocLesson/src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Bloc blocA1 = new Bloc(3, "A45");
+
+	
     }
 }
