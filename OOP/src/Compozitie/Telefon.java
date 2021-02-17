4b6e9a616c104b32d780ff2829b90be06e0efa40
MesseV <vlad.mesesan1@gmail.com>
2021-02-17 18:23:21 +0200

    Create Telefon.java

---
+++ OOP/src/Compozitie/Telefon.java
@@ -0,0 +1,22 @@
+package Compozitie;
+
+public class Telefon {
+
+    private String model;
+    private int memorie;
+    private int diagonala;
+
+    public Telefon (String model) {
+        this.model = model;
+    }
+
+    public void setModel (String model) {
+        this.model = model;
+    }
+
+    public String getModel () {
+        return this.model;
+    }
+
+    
+}
