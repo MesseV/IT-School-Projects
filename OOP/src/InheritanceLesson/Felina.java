2b5db9f05cca04370caffa3036c0ad1e7f0f537a
MesseV <vlad.mesesan1@gmail.com>
2021-02-22 20:19:09 +0200

    small adjustments

--- OOP/src/InheritanceLesson/Felina.java
+++ OOP/src/InheritanceLesson/Felina.java
@@ -3,4 +3,13 @@ package InheritanceLesson;
 public class Felina extends Animal{ //asa se marcheaza inheritance-ul
     //clasa Felina va mosteni toate caracterisiticile si membrii clasei Animal
 
+    private String culoareBlana;
+
+    public String getCuloareBlana() {
+        return culoareBlana;
+    }
+
+    public void setCuloareBlana(String culoareBlana) {
+        this.culoareBlana = culoareBlana;
+    }
 }
