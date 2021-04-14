1cbd1b7a82815c0d911482f58333ee52575e8cac
MesseV <vlad.mesesan1@gmail.com>
2021-04-14 20:31:32 +0300

    more html and database integrations
    thymeleaf, @RequestParam etc etc

--- springInitializer/productmanagement/src/main/java/com/itschool/productmanagement/controller/AuthorController.java
+++ springInitializer/productmanagement/src/main/java/com/itschool/productmanagement/controller/AuthorController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller
@@ -18,13 +21,36 @@ public class AuthorController {
     @GetMapping(path = "author")
     public String displayAuthors (Model model) {
         List<AuthorModel> authorModelList = authorService.displayAuthors();
-        model.addAttribute("title", "Afisare autori:");
         model.addAttribute("authors", authorModelList);
         return "authors";
     }
 
     @GetMapping(path = "add-author")
-    public String addAuthor () {
+    public String viewAuthorPage (Model model) {
+        model.addAttribute("NewAuthor", new AuthorModel());
         return "author-add";
     }
+
+    @GetMapping(path = "author-add")
+    public String addAuthor (@ModelAttribute AuthorModel newAuthor){
+        System.out.println("Add author -> " + newAuthor.getFirstName() + " " + newAuthor.getLastName());
+        authorService.addAuthor(newAuthor);
+        return "redirect:/author";
+    }
+
+    @GetMapping(path = "deleteById")
+    public String deleteById(@RequestParam("id") int id){
+        System.out.println("Deleting author with id: " + id);
+        authorService.deleteById(id);
+        return "redirect:/author";
+    }
+
+    @GetMapping(path = "findById")
+    public String findById(@RequestParam("id") int id, Model model) {
+        System.out.println("Viewing details by id");
+        AuthorModel authorModel = authorService.findById(id);
+        model.addAttribute("foundAuthor", authorModel);
+        return "view-author";
+    }
+
 }
--- springInitializer/productmanagement/src/main/java/com/itschool/productmanagement/services/AuthorService.java
+++ springInitializer/productmanagement/src/main/java/com/itschool/productmanagement/services/AuthorService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class AuthorService {
@@ -16,14 +17,23 @@ public class AuthorService {
 
      @GetMapping(path = "authors")
      public List<AuthorModel> displayAuthors () {
-         System.out.println("Displaying authors:");
+         System.out.println("Displaying authors.");
          List<AuthorModel> authors = authorRepository.findAll();
          return authors;
      }
 
      @GetMapping(path = "author-add")
      public void addAuthor (AuthorModel author) {
-         System.out.println("Adding authors to database.");
+         authorRepository.save(author);
+     }
+
+     public void deleteById (int id) {
+         authorRepository.deleteById(id);
+     }
+
+     public AuthorModel findById(int id) {
+         Optional<AuthorModel> optionalAuthorModel = authorRepository.findById(id);
+         return optionalAuthorModel.get();
      }
 
 }
