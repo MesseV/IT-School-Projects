6be5410c12a97e742a4b1b0682bd9b5e8ea5358c
MesseV <vlad.mesesan1@gmail.com>
2021-04-26 20:43:30 +0300

    A

--- springInitializer/productmanagement/src/main/java/com/itschool/productmanagement/controller/UserController.java
+++ springInitializer/productmanagement/src/main/java/com/itschool/productmanagement/controller/UserController.java
@@ -3,11 +3,13 @@ package com.itschool.productmanagement.controller;
 import com.itschool.productmanagement.entities.UserModel;
 import com.itschool.productmanagement.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
+@Controller
 public class UserController {
 
     @Autowired
