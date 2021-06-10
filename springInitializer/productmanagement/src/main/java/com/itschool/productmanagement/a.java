acc9b1d270cc9c4ad8a4b9aea62a7d7726bdfb54
Vlad Mesesan <vlad.mesesan1@gmail.com>
2021-06-10 08:01:59 +0300

    Create a.java

---
+++ springInitializer/productmanagement/src/main/java/com/itschool/productmanagement/a.java
@@ -0,0 +1,12 @@
+package com.itschool.productmanagement;
+
+public class a {
+    public static void main(String[] args) {
+        boolean y = true;
+        boolean x = y || true;
+        System.out.println(x);
+
+        int [] sir = new int[]{1,2,3,4,5};
+
+    }
+}
